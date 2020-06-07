4b6abad81b0d651062c53eba8fb9dfa832764004
haywhy43 <170805005@live.unilag.edu.ng>
2020-06-08 00:25:46 +0100

    first init

---
+++ studentdb/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace studentdb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
